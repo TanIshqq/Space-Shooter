82fbf4cd0b6478a2a33610556bafd8f54e4e5a8c
Tanishq Goel <tanishqgoel@gmail.com>
2018-06-23 03:34:21 +0530

    Shots Fired

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Boundary{
+    
+    public float xMin, xMax, zMin, zMax;
+}
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    public float rotation;
+    public Boundary b;
+
+    public GameObject shot;
+    public Transform shotSpawn;
+    public float fireRate;
+    private float nextFire;
+
+    void Update()
+    {
+        if (Input.GetButton("Fire1") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+        }
+    }
+
+	void FixedUpdate()
+	{
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        GetComponent<Rigidbody>().velocity = movement*speed;
+
+        GetComponent<Rigidbody>().position = new Vector3(
+
+            Mathf.Clamp(GetComponent<Rigidbody>().position.x,b.xMin,b.xMax),
+            0.0f,
+            Mathf.Clamp(GetComponent<Rigidbody>().position.z, b.zMin, b.zMax)
+
+        );
+
+        GetComponent<Rigidbody>().rotation = Quaternion.Euler(0.0f, 0.0f, GetComponent<Rigidbody>().position.x * -rotation);
+
+
+	}
+}
